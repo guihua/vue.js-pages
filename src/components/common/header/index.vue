86f96afe9e4fc91c674c44bea27b611fb59daebf
guihua.pgh <guihua.pgh@alibaba-inc.com>
2017-08-01 18:02:49 +0800

    新增公用样式：包括多媒体、SCSS 函数、reset 重置样式和基本变量

---
+++ src/components/common/header/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <header class="cmui-header">
+    <h1>{{title}}</h1>
+    <slot></slot>
+  </header>
+</template>
+
+<script>
+  export default {
+    name: 'header',
+
+    props: {
+      title: {
+        type: String,
+        default: '标题'
+      }
+    },
+
+    methods: {
+      back () {
+        history.back()
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  @import './header';
+</style>
