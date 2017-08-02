0c4c52959f6715ad0ffee150d632b70d85e1556e
guihua.pgh <guihua.pgh@alibaba-inc.com>
2017-08-02 15:13:32 +0800

    Iconfont 列表

---
+++ src/views/icon/iconfont.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="page page-iconfont">
+    <iconfont></iconfont>
+  </div>
+</template>
+
+<script>
+  export default {
+    components: {
+      Iconfont: require('../../components/iconlist/icon')
+    }
+  }
+</script>
+
+<style lang="scss">
+  @import '../../assets/styles/base';
+
+  .cmui-icon {
+    color: #f60;
+    font-size: 30px !important;
+  }
+</style>
+
---
+++ src/views/icon/index.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import Iconfont from './iconfont.vue'
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#icon',
+  template: '<Iconfont />',
+  components: {
+    Iconfont
+  }
+})
