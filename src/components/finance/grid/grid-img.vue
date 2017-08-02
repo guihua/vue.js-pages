310b9b738027d05db84fc6287b2966b8ae438452
guihua.pgh <guihua.pgh@alibaba-inc.com>
2017-08-02 12:09:14 +0800

    finance grid 组件

---
+++ src/components/finance/grid/grid-img.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="grid-item">
+    <div class="grid-mask"></div>
+    <img :src="img" alt="">
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'grid-item',
+
+    props: {
+      img: String
+    }
+  }
+</script>
